3faa526fd2c7e7fe2efb3e9f69d83b0d2aae5b04
cvriends <calvinvr29@gmail.com>
2020-07-02 12:44:07 +0200

    first commit

---
+++ functions_a3.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+class flatPlate():
+    """Define a flat plate geometry in a moving reference frame
+    """
+    def __init__(self, N, c):
+        """Initialize with a number of plate elements
+
+        Args:
+            N (int): Number of plate elements
+        """
+        self.N = N
+        self.c = c
+        # Define the flat plate geometry in a moving frame
+        # of reference x, z
+        self.x = np.linspace(0,1, N+1)*self.c
+        self.z = np.zeros((1,N+1))*self.c
+        self.qc_x = np.arange(1/N*1/4,1+1/N*1/4, 1/N) *self.c # quarter chord
+        self.cp_x = np.arange(1/N*3/4,1+1/N*3/4, 1/N) *self.c # collocation points
+        self.qc_z = self.qc_x * 0
+        self.cp_z = self.cp_x * 0
+
+def VOR2D(GAMMA, X, Z, Xj, Zj):
+    """Discrete ortex method (11.1.1 from Katz Plotkin)
+
+    Args:
+        GAMMA (float): Vortex Strength
+        X (float): Velocity induced at this point location  
+        Z (float): Velocity induced at this point location
+        Xj (float): Vortex element located as this location
+        Zj ([type]): Vortex element located at this location
+
+    Returns:
+        numpy array: Velocity induced at X, Z from vortex element Xj,Zj
+    """
+    rj2 = ((X-Xj)**2 + (Z-Zj)**2)
+    output = GAMMA/(2*np.pi*rj2)*np.dot(np.array([[0,1],[-1,0]]),np.array([X-Xj, Z-Zj]))
+    return output
+
+def transform_local_to_global(x,z,theta,X0,Z0):
+    """ Transformation matrix from 13.107
+
+    Args:
+        x (float): Location in moving ref
+        z (float): Location in moving ref
+        theta (float): Rotation angle in radians
+        X0 (float): Flight path
+        Z0 (float): Flight path
+
+    Returns:
+        numpy array: Transformed array
+    """
+    rot = np.array([[np.cos(theta),np.sin(theta)],[-np.sin(theta), np.cos(theta)]])
+    output = np.dot(rot,np.array([x,z])) + np.array([[X0],[Z0]])
+    return output
+
+def transform_velocity_l_to_g(theta, xdot, zdot):
+    """ Transformation matrix from 13.108
+
+    Args:
+        theta (float): Rotation angle in radians
+        xdot (float): x velocity
+        xdot (float): z velocity
+
+    Returns:
+        numpy array: Transformed array
+    """
+    rot = np.array([[np.cos(theta),np.sin(theta)],[-np.sin(theta), np.cos(theta)]])
+    output = np.dot(rot,np.array([[xdot],[zdot]]))
+    return output
---
+++ main.py
@@ -0,0 +1,14 @@
+import numpy as np
+from functions_a3 import flatPlate, VOR2D
+## Definition of Geometry
+fltplt4 = flatPlate(4,1)
+n = np.array([0,1])
+
+inf_coef = np.zeros((fltplt4.N,fltplt4.N))
+
+for i in range(fltplt4.N):
+    for j in range(fltplt4.N):
+        ind_vel = VOR2D(1, fltplt4.cp_x[i], fltplt4.cp_z[i], fltplt4.qc_x[j], fltplt4.qc_z[j])
+        inf_coef[i, j] = np.dot(ind_vel,n)
+
+print(inf_coef)
