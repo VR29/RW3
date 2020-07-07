cff43382b9b01fd8688b5714fe2a23633ed15d02
Joey <joey@erjpd.demon.nl>
2020-07-07 19:27:34 +0200

    Removed outlier, started with pressure distr

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ t_end = 20# sec
 dt = 1 # sec
 
 U_inf = 10
-alpha = np.deg2rad(20)
+alpha = np.deg2rad(5)
 X0_dot = -U_inf
 Z0_dot = 0
 rho = 1.293
@@ -19,14 +19,17 @@ l = fltplt4.c/fltplt4.N
 
 t_array = np.arange(0, t_end+dt, dt)
 
-X_wake = np.zeros(len(t_array))
-Z_wake = np.zeros(len(t_array))
+X_wake = np.zeros(len(t_array)-1)
+Z_wake = np.zeros(len(t_array)-1)
 X_TE = np.zeros(len(t_array)) # X location trailing edge
 Z_TE = np.zeros(len(t_array)) # Z location trailing edge
 X_LE = np.zeros(len(t_array)) # X location leading edge
 Z_LE = np.zeros(len(t_array)) # Z location leading edge
-Gamma_wake = np.zeros(len(t_array))
+Gamma_wake = np.zeros(len(t_array)-1)
 i_t = 0
+
+p_dist_t = np.zeros((len(t_array), fltplt4.N+len(t_array)-1))
+cl_t = np.zeros(len(t_array))
 
 for t in t_array:
     # print("t =",t)
@@ -48,6 +51,11 @@ for t in t_array:
             RHS[i] = -np.dot(inv_transform_velocity_l_to_g(alpha, X0_dot, Z0_dot),n)
 
         gamma = np.linalg.solve(inf_coef, RHS)
+
+        # compute quantities of interest
+        delta_p = rho*U_inf*gamma/l
+        L = np.sum(delta_p*l*np.cos(alpha))
+        cl_t[i_t] = L / (0.5 * rho * U_inf**2 * fltplt4.c)
 
     else:
         ## Calculation of Influence Coefficients
@@ -71,18 +79,19 @@ for t in t_array:
         #print(RHS)
 
         gamma = np.linalg.solve(inf_coef, RHS)
-        Gamma_wake[i_t] = gamma[-1]
+        
+        Gamma_wake[i_t-1] = gamma[-1]
         # print(transform_local_to_global(trailing_vortex,0,alpha,X0_dot*t,Z0_dot*t))
-        X_wake[i_t] = transform_local_to_global(trailing_vortex,0,alpha,X0_dot*t,Z0_dot*t)[0]
-        Z_wake[i_t] = transform_local_to_global(trailing_vortex,0,alpha,X0_dot*t,Z0_dot*t)[1]
-        induced_wake =  np.zeros((2,len(t_array)))
+        X_wake[i_t-1] = transform_local_to_global(trailing_vortex,0,alpha,X0_dot*t,Z0_dot*t)[0]
+        Z_wake[i_t-1] = transform_local_to_global(trailing_vortex,0,alpha,X0_dot*t,Z0_dot*t)[1]
+        induced_wake =  np.zeros((2,len(t_array)-1))
 
-        for i in range(i_t): # Collocation points
+        for i in range(i_t-1): # Collocation points
             for j in range(fltplt4.N): # Quarter chord vortex elements
                 # print(transform_local_to_global(fltplt4.qc_x[j],fltplt4.qc_z[j],alpha,X0_dot*t,Z0_dot*t))
                 airfoil_X, airfoil_Z = transform_local_to_global(fltplt4.qc_x[j],fltplt4.qc_z[j],alpha,X0_dot*t,Z0_dot*t)
                 induced_wake[:,i] += VOR2D(gamma[j], X_wake[i], Z_wake[i], airfoil_X, airfoil_Z) # Airfoil induced on wake
-            for k in range(i_t):
+            for k in range(i_t-1):
                 if k == i:
                     continue
                 induced_wake[:,i] += VOR2D(Gamma_wake[k], X_wake[i], Z_wake[i], X_wake[k], Z_wake[k]) # Self induced
@@ -90,6 +99,12 @@ for t in t_array:
         X_wake += induced_wake[0,:] * dt
         Z_wake += induced_wake[1,:] * dt
         #print(gamma)
+
+        # compute quantities of interest (cl with gamma over airfoil only)
+        delta_p = rho*U_inf*gamma[:fltplt4.N]/l
+        L = np.sum(delta_p*l*np.cos(alpha))
+        cl_t[i_t] = L / (0.5 * rho * U_inf**2 * fltplt4.c)
+        p_dist_t[i_t, :] = rho*U_inf*np.hstack((np.squeeze(gamma[:fltplt4.N]), Gamma_wake))/l
     
     X_LE[i_t], Z_LE[i_t] = transform_local_to_global(0,0,alpha,X0_dot*t,Z0_dot*t)
     X_TE[i_t], Z_TE[i_t] = transform_local_to_global(fltplt4.x[-1],0,alpha,X0_dot*t,Z0_dot*t)
@@ -103,7 +118,13 @@ for t in t_array:
 fig, ax = plt.subplots()
 ax.plot(X_LE,Z_LE, '.g')
 ax.plot(X_TE,Z_TE, '.r')
-ax.plot(X_wake, Z_wake, '.')
-ax.plot(X_wake, Z_wake, '.')
-plt.show()
+ax.plot(X_wake, Z_wake, 'x')
+ax.plot(X_wake, Z_wake, 'x')
 
+fig, ax = plt.subplots(2,1)
+ax[0].plot(t_array, cl_t)
+for i in range(p_dist_t.shape[0]):
+    ax[1].plot(np.hstack((fltplt4.cp_x, X_wake)), p_dist_t[i], "x", label=str(i))
+ax[1].legend()
+
+plt.show()
